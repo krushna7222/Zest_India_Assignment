0b377e4d591ea562e7ad86b4dddd221b1d1553d1
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2026-02-21 09:22:05 +0530

    updated

--- Zest_Java_Backend_Assignment/src/main/java/com/zest/assignment/controller/AdminController.java
+++ Zest_Java_Backend_Assignment/src/main/java/com/zest/assignment/controller/AdminController.java
@@ -24,35 +24,35 @@ public class AdminController {
 	@Autowired
     private AdminService service;
 
-	 @PostMapping("/login")
-	    public ResponseEntity<ApiResponse<AuthResponse>> login(@RequestBody LoginRequest request) {
-
-	        AuthResponse response = service.login(request);
-
-	        return ResponseEntity.ok(
-	                new ApiResponse<>(200, response, "Login successful")
-	        );
-	    }
-
-	    @PostMapping("/logout")
-	    public ResponseEntity<ApiResponse<String>> logout(@RequestParam String refreshToken) {
-
-	        service.logout(refreshToken);
-
-	        return ResponseEntity.ok(
-	                new ApiResponse<>(200, null, "Logout successful")
-	        );
-	    }
-	    
-	    @PostMapping("/refresh-token")
-	    public ResponseEntity<ApiResponse<AuthResponse>> refreshToken(@RequestBody RefreshRequest request) {
-
-	        AuthResponse response = service.refreshToken(request);
-
-	        return ResponseEntity.ok(
-	                new ApiResponse<>(200, response, "Token refreshed successfully")
-	        );
-	    }
+//	 @PostMapping("/login")
+//	    public ResponseEntity<ApiResponse<AuthResponse>> login(@RequestBody LoginRequest request) {
+//
+//	        AuthResponse response = service.login(request);
+//
+//	        return ResponseEntity.ok(
+//	                new ApiResponse<>(200, response, "Login successful")
+//	        );
+//	    }
+//
+//	    @PostMapping("/logout")
+//	    public ResponseEntity<ApiResponse<String>> logout(@RequestParam String refreshToken) {
+//
+//	        service.logout(refreshToken);
+//
+//	        return ResponseEntity.ok(
+//	                new ApiResponse<>(200, null, "Logout successful")
+//	        );
+//	    }
+//	    
+//	    @PostMapping("/refresh-token")
+//	    public ResponseEntity<ApiResponse<AuthResponse>> refreshToken(@RequestBody RefreshRequest request) {
+//
+//	        AuthResponse response = service.refreshToken(request);
+//
+//	        return ResponseEntity.ok(
+//	                new ApiResponse<>(200, response, "Token refreshed successfully")
+//	        );
+//	    }
 	   
     
     
